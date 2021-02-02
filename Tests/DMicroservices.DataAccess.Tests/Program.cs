2343325778b00015704ba1d2fd3e2f9ff283690d
Duhan Taha BULAK <duhan.bulak@detaysoft.com>
2021-02-02 13:15:48 +0300

    Log template test

--- Tests/DMicroservices.DataAccess.Tests/Program.cs
+++ Tests/DMicroservices.DataAccess.Tests/Program.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using StackExchange.Redis;
+using DMicroservices.Utils.Logger;
 
 namespace DMicroservices.DataAccess.Tests
 {
@@ -23,69 +24,85 @@ namespace DMicroservices.DataAccess.Tests
             //    var document = repo.GetAll(x => true).ToList();
             //}
 
+            //try
+            //{
+            //    using (var uow = UnitOfWorkFactory.CreateUnitOfWork<MasterContext>(new UnitOfWorkSettings()
+            //    {
+            //        ChangeDataCapture = true,
+            //        ChangedUserPropertyName = "ChangedBy",
+            //        IdPropertyName = "Id"
+            //    }))
+            //    {
+            //        var ct = uow.GetRepository<City>().Get(x => x != null);
+
+            //        uow.GetRepository<City>().Delete(ct);
+            //        uow.SaveChanges();
+            //    }
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+
+            //return;
+
+            //using (UnitOfWork<MasterContext> uow = new UnitOfWork<MasterContext>())
+            //{
+            //    var city = new City() { Name = "sivas" };
+            //    var t1 = new Teacher() { Branch = 1, Name = "serhat", City = city };
+            //    var t2 = new Teacher() { Branch = 2, Name = "süha", City = city };
+
+            //    var s1 = new Student() { StudentNum = 5858, Name = "emre", City = city };
+            //    var s2 = new Student() { StudentNum = 5860, Name = "duhan", City = city };
+
+
+            //    uow.GetRepository<City>().Add(city);
+
+            //    uow.GetRepository<Teacher>().Add(t1);
+            //    uow.GetRepository<Teacher>().Add(t2);
+
+            //    uow.GetRepository<Student>().Add(s1);
+            //    uow.GetRepository<Student>().Add(s2);
+
+            //    uow.SaveChanges();
+
+            //    var persons = uow.GetRepository<City>().Get(x => x.Id.Equals(1), new List<string>() { "Persons" })
+            //        .Persons;
+
+            //    foreach (var person in persons)
+            //    {
+
+            //        if (person is Student)
+            //        {
+
+            //        }
+            //        else if (person is Teacher)
+            //        {
+            //            var teacher = (Teacher)person;
+            //        }
+            //    }
+
+            //}
+
+            ElasticLogger.Instance.Info("Info Log", new Dictionary<string, Object>() {
+                    { "No", 1 },
+                    { "Name", "DMicroServices Info" }
+                });
+
             try
             {
-                using (var uow = UnitOfWorkFactory.CreateUnitOfWork<MasterContext>(new UnitOfWorkSettings()
-                {
-                    ChangeDataCapture = true,
-                    ChangedUserPropertyName = "ChangedBy",
-                    IdPropertyName = "Id"
-                }))
-                {
-                    var ct = uow.GetRepository<City>().Get(x => x != null);
-                  
-                    uow.GetRepository<City>().Delete(ct);
-                    uow.SaveChanges();
-                }
-
+                int a = 0;
+                int b = 10 / a;
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                Console.WriteLine(e);
+                ElasticLogger.Instance.Error(ex, "Exception Log", new Dictionary<string, Object>() {
+                    { "No", 1 },
+                    { "Name", "DMicroServices Error" }
+                });
             }
-           
-
-            return;
-            
-            using (UnitOfWork<MasterContext> uow = new UnitOfWork<MasterContext>())
-            {
-                var city = new City() { Name = "sivas" };
-                var t1 = new Teacher() { Branch = 1, Name = "serhat", City = city };
-                var t2 = new Teacher() { Branch = 2, Name = "süha", City = city };
-
-                var s1 = new Student() { StudentNum = 5858, Name = "emre", City = city };
-                var s2 = new Student() { StudentNum = 5860, Name = "duhan", City = city };
-
-
-                uow.GetRepository<City>().Add(city);
-
-                uow.GetRepository<Teacher>().Add(t1);
-                uow.GetRepository<Teacher>().Add(t2);
-
-                uow.GetRepository<Student>().Add(s1);
-                uow.GetRepository<Student>().Add(s2);
-
-                uow.SaveChanges();
-
-                var persons = uow.GetRepository<City>().Get(x => x.Id.Equals(1), new List<string>() { "Persons" })
-                    .Persons;
-
-                foreach (var person in persons)
-                {
-
-                    if (person is Student)
-                    {
-
-                    }
-                    else if (person is Teacher)
-                    {
-                        var teacher = (Teacher)person;
-                    }
-                }
-
-            }
-
-
             Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
