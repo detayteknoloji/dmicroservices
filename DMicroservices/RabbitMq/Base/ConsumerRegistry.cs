9352ab111447e5939ae573626c79284ef77101cb
gökhan anıl koç <anil.koc@detaysoft.com>
2023-02-27 10:14:45 +0300

    using clear

--- DMicroservices/RabbitMq/Base/ConsumerRegistry.cs
+++ DMicroservices/RabbitMq/Base/ConsumerRegistry.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using DMicroservices.RabbitMq.Consumer;
-using DMicroservices.RabbitMq.Producer;
 using DMicroservices.Utils.Logger;
-using MongoDB.Driver;
 
 namespace DMicroservices.RabbitMq.Base
 {
